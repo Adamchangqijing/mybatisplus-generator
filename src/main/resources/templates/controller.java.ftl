25f1f1fbb992c1d36e1dab565d8a91bf2461faa5
昌齐晶 <changqijing@zhuiyinanian.com>
2019-04-04 18:48:28 +0800

    init

---
+++ src/main/resources/templates/controller.java.ftl
@@ -0,0 +1,43 @@
+package ${package.Controller};
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+
+<#if restControllerStyle>
+import org.springframework.web.bind.annotation.RestController;
+<#else>
+import org.springframework.stereotype.Controller;
+</#if>
+<#if superControllerClassPackage??>
+import ${superControllerClassPackage};
+</#if>
+
+/**
+ * <p>
+ * ${table.comment} 前端控制器
+ * </p>
+ *
+ * @author ${author}
+ * @since ${date}
+ */
+<#if restControllerStyle>
+@RestController
+<#else>
+@Controller
+</#if>
+@Api
+@RequestMapping("<#if package.ModuleName??>/${package.ModuleName}</#if>/<#if controllerMappingHyphenStyle??>${controllerMappingHyphen}<#else>${table.entityPath}</#if>")
+@Slf4j
+<#if kotlin>
+class ${table.controllerName}<#if superControllerClass??> : ${superControllerClass}()</#if>
+<#else>
+<#if superControllerClass??>
+public class ${table.controllerName} extends ${superControllerClass} {
+<#else>
+public class ${table.controllerName} {
+
+}
+</#if>
+</#if>
---
+++ src/test/java/com/baomidou/springboot/test/generator/GeneratorServiceEntity.java
@@ -0,0 +1,81 @@
+package com.baomidou.springboot.test.generator;
+
+import org.junit.Test;
+
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
+import com.baomidou.mybatisplus.generator.config.GlobalConfig;
+import com.baomidou.mybatisplus.generator.config.PackageConfig;
+import com.baomidou.mybatisplus.generator.config.StrategyConfig;
+import com.baomidou.mybatisplus.generator.config.rules.DbType;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+
+/**
+ * <p>
+ * 测试生成代码
+ * </p>
+ *
+ * @author K神
+ * @date 2017/12/18
+ */
+public class GeneratorServiceEntity {
+
+    @Test
+    public void generateCode() {
+        String packageName = "com.ioun.asset.server";
+        boolean serviceNameStartWithI = false;//user -> UserService, 设置成true: user -> IUserService
+        generateByTables(serviceNameStartWithI, packageName, "asset_fund_order");
+    }
+
+    private void generateByTables(boolean serviceNameStartWithI, String packageName, String... tableNames) {
+        GlobalConfig config = new GlobalConfig();
+        String dbUrl = "jdbc:mysql://120.0.0.0:3306/asset";
+        DataSourceConfig dataSourceConfig = new DataSourceConfig();
+        dataSourceConfig.setDbType(DbType.MYSQL)
+                .setUrl(dbUrl)
+                .setUsername("root")
+                .setPassword("password")
+                .setDriverName("com.mysql.jdbc.Driver");
+        StrategyConfig strategyConfig = new StrategyConfig();
+        strategyConfig
+                //.setCapitalMode(true)
+                .setSuperEntityClass("com.ioun.common.config.base.BaseEntity")
+                .setSuperEntityColumns(new String[] {})
+                .setSuperMapperClass("com.ioun.common.config.base.BaseMapper")
+                .setSuperServiceClass("com.ioun.common.config.service.BaseService")
+                .setSuperServiceImplClass("com.ioun.common.config.service.impl.BaseServiceImpl")
+//                .setSuperControllerClass("com.zynn.common.core.base.BaseController")
+                .setRestControllerStyle(true)//设置成restController
+                .setEntityLombokModel(true)
+                .setDbColumnUnderline(true)
+                .setNaming(NamingStrategy.underline_to_camel)
+                .setInclude(tableNames);//修改替换成你需要的表名，多个表名传数组
+
+        config.setActiveRecord(false)
+                .setAuthor("刘猛")
+                .setOutputDir("d:\\codeGen")
+                .setFileOverride(true)
+                .setEnableCache(false);
+        if (!serviceNameStartWithI) {
+            config.setServiceName("%sService");
+            config.setControllerName("%sServiceImpl");
+        }
+        new AutoGenerator().setGlobalConfig(config)
+                .setDataSource(dataSourceConfig)
+                .setStrategy(strategyConfig)
+                .setPackageInfo(
+                        new PackageConfig()
+                                .setParent(packageName)
+                                .setService("bisiness.service")
+                                .setServiceImpl("bisiness.service.impl")
+                                .setController("service")
+                                .setEntity("bisiness.entity")
+                                .setMapper("bisiness.persistence.dao")
+                                .setXml("bisiness.persistence.mapper")
+                ).execute();
+    }
+
+    private void generateByTables(String packageName, String... tableNames) {
+        generateByTables(true, packageName, tableNames);
+    }
+}
